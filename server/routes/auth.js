0967237d03bdd7872098c2a6453d3e53411abe49
swaglord2021 <nuoya1996@gmail.com>
2023-01-07 00:15:59 -0500

    added server code for login and register.

---
+++ server/routes/auth.js
@@ -0,0 +1,41 @@
+const User = require('../model/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const express = require('express');
+const router = express.Router();
+
+const handleLogin = async(req, res) => {
+    const username = req.body.username;
+    const pwd = req.body.password;
+    const foundUser = await User.findOne({ username: username }).exec();
+    const match = await bcrypt.compare(pwd, foundUser.password);
+
+    if (!foundUser) return res.sendStatus(400); //Unauthorized
+    if (match) {
+        const accessToken = jwt.sign(
+            {
+                "User": {
+                    "username": foundUser.username
+                }
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '30s' }
+        );
+
+        const refreshToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        )
+
+        foundUser.refreshToken = refreshToken;
+        foundUser.save()
+            .then(() => res.json({ accessToken, refreshToken }))
+            .catch(err => res.status(400).json('Error' + err));
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+router.post('/', handleLogin);
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const cors = require('cors');
+
+const PORT = process.env.PORT || 4000;
+const URI = process.env.ATLAS_URI;
+const connectDB = () => {
+    const connection = mongoose.connection;
+    mongoose.set('strictQuery', true);
+    mongoose.connect(URI);
+
+    connection.once('open', () => console.log('connection to DB established!'));
+    connection.on('error', err => console.log(err));
+}
+
+app.use(express.json());
+connectDB();
+
+app.use('/auth', require('./routes/auth'));
+app.use('/register', require('./routes/register'));
+
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+
